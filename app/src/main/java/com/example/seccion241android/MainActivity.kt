0472e3d089c16e6736905229a210a737d4163fcd
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2022-10-24 14:48:52 -0300

    firebase

--- app/src/main/java/com/example/seccion241android/MainActivity.kt
+++ app/src/main/java/com/example/seccion241android/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.seccion241android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import com.example.seccion241android.datos.Base
 import com.example.seccion241android.models.Producto
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +19,30 @@ class MainActivity : AppCompatActivity() {
         val producto = Producto("Raid",79)
         val conexion = Base(this,null,null, null)
         conexion.insertProducto(producto)
+        var mensaje: TextView = findViewById(R.id.main_Mensaje)
+
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+
+        // Read from the database
+        myRef.addValueEventListener(object: ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.getValue()
+                Log.d("EL VALOR", "Value is: " + value)
+                mensaje.text = value.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("EL VALOR", "Failed to read value.", error.toException())
+            }
+
+        })
+
     }
 }
